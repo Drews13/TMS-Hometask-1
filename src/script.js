4bcca1db69a34e64acacbb52061002d8cb67979d
Drews <amartinovich.01@gmail.com>
2023-04-22 02:47:55 +0300

    Added Burger+Slider

---
+++ src/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 20,
+    direction: 'horizontal',
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+});
